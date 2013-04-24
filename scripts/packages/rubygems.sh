aa38a9f9cb580233000491ecf9cfd904c6d0a0d4
Maxime Fabre <ehtnam6@gmail.com>
2013-04-24 16:47:21 +0200

    Fix script

--- scripts/packages/rubygems.sh
+++ scripts/packages/rubygems.sh
@@ -1,8 +1,12 @@
-gems=("sass --pre" "compass --pre" "genghisapp --pre" "susy" "bson_ext")
+gems=("sass" "compass" "genghisapp" "susy" "bson_ext")
 
 for gem in "${gems[@]}"
 do
-  if $(gem list $gem -i); then
-    gem install $gem
+  if ! $(gem list $gem -i); then
+    if [[ $gem = "sass" ]] || [[ $gem = "compass" ]] || [[ $gem = "genghisapp"; ]] then
+      gem install $gem --pre
+    else
+      gem install $gem
+    fi
   fi
 done
