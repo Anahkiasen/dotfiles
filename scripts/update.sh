47c91a249bdee5d8dfcb74ec164a610e48be8db2
Maxime Fabre <ehtnam6@gmail.com>
2013-04-26 15:48:55 +0200

    Add upgrade command

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,16 +12,19 @@ source $PACKAGES/npm.sh
 header "Update package managers"
 
 # Rubygems
+subheader "Updating Rubygems"
 sudo gem update
 sudo gem update --system
 sudo gem cleanup
 
 # Node Package Manager
+info "Updating NPM"
 sudo npm update -g
 sudo npm update npm -g
 sudo npm cache clean
 
 # Composer and PHP-CS-Fixer
+info "Updating Composer"
 sudo composer self-update
 sudo php-cs-fixer self-update
 
@@ -29,9 +32,11 @@ cd /usr/local/composer
 sudo composer update
 
 # RVM
+info "Updating Ruby"
 sudo rvm get latest
 
 # Homebrew
+info "Updating Homebrew"
 brew update
 brew upgrade
 brew cleanup
@@ -41,12 +46,14 @@ brew cleanup
 header "Update PHPMyAdmin and Oh My Zsh"
 
 # PHPMyAdmin
+info "Updating PHPMyAdmin"
 cd /Applications/MAMP/bin/phpMyAdmin
 git reset --hard -q
 git fetch origin master -q
 git pull origin master
 
 # Oh My Zsh
+info "Updating Oh My ZSH"
 cd "$HOME/.oh-my-zsh/"
 git reset --hard -q
 git fetch origin master -q
---
+++ scripts/upgrade.sh
@@ -0,0 +1,15 @@
+header "Updating core files"
+
+# Update core repository
+cd $DOTFILES
+git fetch -q origin master
+git pull -q origin master
+
+# Synchronize folders
+rsync --exclude ".git/" --exclude "scripts" --exclude "bootstrap.sh" --exclude "README.md" -a . ~
+success "Scripts and profiles updated"
+
+# Move binary
+chmod +x ~/dotfiler
+mv ~/dotfiler /usr/local/bin/dotfiler
+success "Binary updated"
