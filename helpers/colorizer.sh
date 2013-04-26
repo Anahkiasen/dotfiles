ac0bbf2ba263f3f68a6161a731d5c0c607df2efa
Maxime Fabre <ehtnam6@gmail.com>
2013-04-26 15:15:07 +0200

    Better headers

--- helpers/colorizer.sh
+++ helpers/colorizer.sh
@@ -12,8 +12,20 @@ function info {
 }
 
 function header {
+  text=$(echo $1 | tr '[:lower:]' '[:upper:]')
+
+  # Get text size
+  offset=10
+  size=$((${#text} + $offset))
+  halfSize=$((($size-$offset) / 4))
+
+  # Create docblocks
+  block=`perl -e "print '/' x $size;"`
+  halfBlock=`perl -e "print '/' x $halfSize;"`
+
   echo -e "\033[34m
-////////////////////////////////////////
-// $1
-////////////////////////////////////////\033[0m"
+$block
+$halfBlock $text $halfBlock
+$block
+\033[0m"
 }
