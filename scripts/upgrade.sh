741b7df7bf403f0abeb4f8ab9fb61aa87bdabfd7
Maxime Fabre <ehtnam6@gmail.com>
2013-05-02 02:35:09 +0200

    Fix stuff

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -6,8 +6,15 @@ git fetch -q origin master
 git pull -q origin master
 
 # Synchronize folders
-rsync --exclude ".git/" --exclude "scripts" --exclude "helpers" --exclude "dotfiler" --exclude "README.md" -a . ~
+rsync --exclude ".git/" --exclude "scripts" --exclude "helpers" --exclude "dotfiler" --exclude "packception" --exclude "README.md" -a . ~
 success "Scripts and profiles updated"
+
+# Update Packception
+subheader "Update Packception"
+
+cd packception
+git fetch -q origin master
+git pull -q origin master
 
 # Update Apache configuration
 rsync -a $VENDOR/apache/ /private/etc/apache2/
