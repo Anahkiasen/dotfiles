0784c02fe2920ad2ca10ed1c2df2ca5bef4d6639
Maxime Fabre <ehtnam6@gmail.com>
2013-05-04 00:54:28 +0200

    Add Bootstrap to repositories to update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,19 @@
 
+update_repository() {
+  folder=$1
+  branch=${2-master}
+  cd "$folder"
+  git reset --hard -q
+  git fetch origin $branch -q
+  git pull origin $branch
+}
+
 # Sync packages ---------------------------------------------------- /
 
 header "Syncing package managers"
 
-packception upgrade
-packception update
+#packception upgrade
+#packception update
 
 # Update some repositories ----------------------------------------- /
 
@@ -12,14 +21,13 @@ header "Update PHPMyAdmin and Oh My Zsh"
 
 # PHPMyAdmin
 subheader "Updating PHPMyAdmin"
-cd /Applications/MAMP/bin/phpMyAdmin
-git reset --hard -q
-git fetch origin master -q
-git pull origin master
+update_repository "/Applications/MAMP/bin/phpMyAdmin"
+
+# Bootstrap
+subheader "Updating Bootstrap"
+update_repository "/Users/anahkiasen/Dropbox/WEB DESIGN/_github/bootstrap" "3.0.0-wip"
+jekyll
 
 # Oh My Zsh
 subheader "Updating Oh My ZSH"
-cd "$HOME/.oh-my-zsh/"
-git reset --hard -q
-git fetch origin master -q
-git pull origin master
+update_repository "$HOME/.oh-my-zsh/"
