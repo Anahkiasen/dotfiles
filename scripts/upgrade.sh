e22de55a4c37577040e0a34e9a35d5026b634474
Maxime Fabre <ehtnam6@gmail.com>
2013-05-03 18:40:35 +0200

    Update excluded folders

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -6,7 +6,7 @@ git fetch -q origin master
 git pull -q origin master
 
 # Synchronize folders
-rsync --exclude ".git/" --exclude "scripts" --exclude "helpers" --exclude "dotfiler" --exclude "packception" --exclude "README.md" -a . ~
+rsync --exclude ".git/" --exclude "scripts" --exclude "lib" --exclude "dotfiler" --exclude "vendor" --exclude "README.md" -a . ~
 success "Scripts and profiles updated"
 
 # Update Apache configuration
