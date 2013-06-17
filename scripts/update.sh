344296b157d91300a7271c99299bee368c9ef3ed
Maxime Fabre <ehtnam6@gmail.com>
2013-06-17 11:00:34 +0200

    Move some stuff around

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,36 +1,10 @@
 
 # Sync packages ---------------------------------------------------- /
 
-header "Syncing package managers"
+header "Update package managers"
 
 packception upgrade
 packception update
-
-# Sync application preference -------------------------------------- /
-
-# Cyberduck
-info "Syncing Cyberduck bookmarks"
-sync_preferences("Cyberduck")
-
-# SourceTree
-info "Syncing SourceTree bookmarks"
-sync_preferences("SourceTree")
-
-# Sublime Text 3
-info "Syncing Sublime Text 3 settings"
-sync_preferences("Sublime\ Text\ 3/Packages")
-sync_preferences("Sublime\ Text\ 3/Installed\ Packages")
-
-# Update configurations -------------------------------------------- /
-
-# Update Apache configuration
-sudo cp /private/etc/apache2/httpd.conf $VENDOR/apache/httpd.conf
-
-# Update PHP configuration
-sudo cp /usr/local/etc/php/5.4/php.ini $VENDOR/php/php.ini
-
-# Restart Apache
-sudo apachectl -k restart
 
 # Update some repositories ----------------------------------------- /
 
--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,5 +1,7 @@
 header "Updating core files"
 
+# Update Dotfiler -------------------------------------------------- /
+
 # Update core repository
 cd $DOTFILES
 git fetch -q origin master
@@ -9,17 +11,37 @@ git pull -q origin master
 rsync --exclude ".git/" --exclude "helpers" --exclude "scripts" --exclude "dotfiler" --exclude "vendor" --exclude "README.md" -a . ~
 success "Scripts and profiles updated"
 
-# Update Apache configuration
-success "Apache configuration updated"
-rsync --exclude "hosts" -a $VENDOR/apache/ /private/etc/apache2/
-sudo cp $VENDOR/apache/hosts /private/etc/hosts
-sudo apachectl -k restart
-
-# Update PHP configuration
-success "PHP configuration updated"
-sudo cp $VENDOR/php/php.ini /usr/local/etc/php/5.4/php.ini
-
 # Move binary
 cp $DOTFILES/dotfiler /usr/local/bin/dotfiler
 chmod +x /usr/local/bin/dotfiler
 success "Binary updated"
+
+# Sync application preferences ------------------------------------- /
+
+# Cyberduck
+info "Syncing Cyberduck bookmarks"
+sync_preferences("Cyberduck")
+
+# SourceTree
+info "Syncing SourceTree bookmarks"
+sync_preferences("SourceTree")
+
+# Sublime Text 3
+info "Syncing Sublime Text 3 settings"
+sync_preferences("Sublime\ Text\ 3/Packages")
+sync_preferences("Sublime\ Text\ 3/Installed\ Packages")
+
+# Update Apache and PHP configuration ------------------------------ /
+
+# Update Apache configuration
+rsync --exclude "hosts" -a $VENDOR/apache/ /private/etc/apache2/
+sudo cp $VENDOR/apache/hosts /private/etc/hosts
+sudo cp /private/etc/apache2/httpd.conf $VENDOR/apache/httpd.conf
+success "Apache configuration updated"
+
+# Update PHP configuration
+sudo cp $VENDOR/php/php.ini /usr/local/etc/php/5.4/php.ini
+success "PHP configuration updated"
+
+# Restart Apache
+sudo apachectl -k restart
