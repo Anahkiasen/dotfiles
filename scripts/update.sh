856cc807b227116f0aca5b52e2e8fb4b39343a88
Maxime Fabre <ehtnam6@gmail.com>
2013-08-03 11:44:54 +0200

    Remove Bootstrap from script

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,11 +9,6 @@ update_repository "$WORK/_apaxy"
 cp $WORK/_apaxy/apaxy/htaccess.txt $WORK/.htaccess
 perl -pi -e 's#/{FOLDERNAME}#http://localhost/_apaxy/apaxy#' $WORK/.htaccess
 
-# Bootstrap
-subheader "Updating Bootstrap"
-update_repository "/Users/anahkiasen/Dropbox/WEB DESIGN/_github/bootstrap" "3.0.0-wip"
-jekyll build
-
 # Oh My Zsh
 subheader "Updating Oh My ZSH"
 update_repository "$HOME/.oh-my-zsh/"
