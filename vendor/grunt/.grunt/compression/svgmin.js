bab819e58db96264027a2a7f7da9c1c56bb8ede8
Maxime Fabre <ehtnam6@gmail.com>
2014-05-26 18:00:45 +0200

    Remove dest

--- vendor/grunt/.grunt/compression/svgmin.js
+++ vendor/grunt/.grunt/compression/svgmin.js
@@ -2,7 +2,6 @@ module.exports = {
 	dist: {
 		expand : true,
 		src    : ['<%= paths.original.svg %>/**/*.svg', '<%= paths.original.img %>/**/*.svg'],
-		dest   : '<%= paths.original.svg %>',
 		ext    : '.svg'
 	}
 };
